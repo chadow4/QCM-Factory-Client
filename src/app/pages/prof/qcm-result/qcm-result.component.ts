a5e52f8fd6262c9e9168ff874da2bf5ae2b24800
julien <chadow.video@gmail.com>
2023-07-20 01:33:12 +0200

    feat : create qcm-result page

--- src/app/pages/prof/qcm-result/qcm-result.component.ts
+++ src/app/pages/prof/qcm-result/qcm-result.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ResultDto} from "../../../models/result.model";
+import {AlertService} from "../../../services/alert.service";
+import {ResultService} from "../../../services/result.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-qcm-result',
   templateUrl: './qcm-result.component.html',
   styleUrls: ['./qcm-result.component.scss']
 })
-export class QcmResultComponent {
+export class QcmResultComponent implements OnInit {
+
+  results!: ResultDto[];
+  idQuestionnaire!: number;
+
+  constructor(private router: Router,
+              private resultService: ResultService,
+              private alertService: AlertService,
+              private route: ActivatedRoute) {
+
+  }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.idQuestionnaire = Number(params.get('id'));
+    });
+    this.resultService.getAllResultsForQuestionnaire(this.idQuestionnaire).subscribe(res => {
+      this.results = res;
+      console.log(res);
+    });
+  }
 
 }
