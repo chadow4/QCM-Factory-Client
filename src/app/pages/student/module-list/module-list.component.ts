9b3f2139a9756acd882fd968b2c37eaa6da728d4
julien <chadow.video@gmail.com>
2023-07-21 03:06:37 +0200

    feat ; create module-list page

--- src/app/pages/student/module-list/module-list.component.ts
+++ src/app/pages/student/module-list/module-list.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ModuleDto} from "../../../models/module.model";
+import {Router} from "@angular/router";
+import {AlertService} from "../../../services/alert.service";
+import {ModuleService} from "../../../services/module.service";
 
 @Component({
   selector: 'app-qcm-list',
   templateUrl: './module-list.component.html',
   styleUrls: ['./module-list.component.scss']
 })
-export class ModuleListComponent {
+export class ModuleListComponent implements OnInit {
+  modules!: ModuleDto[];
+
+  constructor(private router: Router,
+              private alertService: AlertService,
+              private moduleService: ModuleService) {
+  }
+
+  ngOnInit(): void {
+    this.moduleService.getAllModules().subscribe(res => {
+      this.modules = res;
+    })
+  }
+
 
 }
+
+
