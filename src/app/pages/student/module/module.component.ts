99c71f98b545c46bef74159dae927a2abce0f9d9
julien <chadow.video@gmail.com>
2023-07-21 03:07:00 +0200

    feat : create module details page

--- src/app/pages/student/module/module.component.ts
+++ src/app/pages/student/module/module.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {ModuleService} from "../../../services/module.service";
+import {AlertService} from "../../../services/alert.service";
+import {ModuleDto} from "../../../models/module.model";
 
 @Component({
   selector: 'app-modulel',
   templateUrl: './module.component.html',
   styleUrls: ['./module.component.scss']
 })
-export class ModuleComponent {
+export class ModuleComponent implements OnInit {
+
+  module!: ModuleDto;
+  moduleId!: number;
+
+  constructor(private router: Router,
+              private alertService: AlertService,
+              private moduleService: ModuleService,
+              private route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.moduleId = Number(params['id']);
+    });
+    this.moduleService.getModuleById(this.moduleId).subscribe({
+      next: (res) => {
+        this.module = res;
+      },
+      error: (err) => {
+        this.router.navigate(['/module/list']).then(() => this.alertService.error(err.error.message));
+      }
+    });
+  }
 
 }
